f15e8e500cc96c92cc3ef0ef6d26ae8e0e4160ba
ya-regedit <aat-3@yandex.ru>
2021-12-17 19:04:32 +0500

    Сделана заготовка класса Knight И отрисовка карты

---
+++ maps/Ледяной уровень пол.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Ледяной уровень пол" tilewidth="30" tileheight="30" tilecount="20" columns="5">
+ <image source="Ледяной уровень пол.jpg" width="150" height="120"/>
+ <wangsets>
+  <wangset name="Безымянный набор" type="mixed" tile="-1">
+   <wangcolor name="Шаблон1" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="2" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="6" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="7" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="11" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="12" wangid="0,1,1,1,1,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
